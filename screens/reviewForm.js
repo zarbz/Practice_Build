d7ba00e4d4bbd5e3163ac72aa5f6a847e3f8f662
zarbz <div.zarbo@gmail.com>
2020-04-29 14:15:55 -0700

    inital commit

---
+++ screens/reviewForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+    title: yup.string
+        .required()
+        .min(4),
+    body: yup.string()
+        .required
+        .min(8),
+    rating: yup.string()
+        .required()
+        .test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
+            return parseInt(val) <6 && parseInt(val) > 0;
+        })
+})
+
+export default function ReviewForm({ addReview }) {
+
+    return(
+        <View style={globalStyles.container}>
+            <Formik
+                initialValues={{ title: '', body: '', rating: ''}}
+                validationSchema={reviewSchema}
+                onSubmit={(values, actions) => {
+                    actions.resetForm();
+                    addReview(values);
+                    // console.log(values);
+                }}
+            >
+                {(props) => (
+                    <View>
+                        <TextInput 
+                            style={globalStyles.input}
+                            placeholder='Review Title'
+                            onChangeText={props.handleChange('title')}
+                            value={props.values.title}
+                        />
+                        <TextInput
+                            multiline 
+                            style={globalStyles.input}
+                            placeholder='Review Body'
+                            onChangeText={props.handleChange('body')}
+                            value={props.values.body}
+                        />
+                        <TextInput 
+                            style={globalStyles.input}
+                            placeholder='Rating (1-5)'
+                            onChangeText={props.handleChange('rating')}
+                            value={props.values.rating}
+                            keyboardType='numeric'
+                        />
+
+                        <Button title='submit' color='maroon' onPress={props.handleSubmit} />
+                    </View>
+                )}
+            </Formik>
+
+        </View>
+    )
+
+}
